20dd17fb38ec36ab5b3a16f03385d6f315455951
cmdoret <cyril.mattheydoret@gmail.com>
2021-04-05 17:59:45 +0200

    fix extension of glove embedding file

--- toxic_comments/__init__.py
+++ toxic_comments/__init__.py
@@ -5,7 +5,7 @@ import pandas as pd
 
 PROJECT_DIR = pathlib.Path(__file__).parents[1]
 EMBEDDING_FILE_FASTTEXT = PROJECT_DIR / "input/fasttext-crawl-300d-2M.vec"
-EMBEDDING_FILE_TWITTER = PROJECT_DIR / "input/glove.twitter.27B.200d.txt"
+EMBEDDING_FILE_TWITTER = PROJECT_DIR / "input/glove.twitter.27B.200d.vec"
 
 training_set = pd.read_csv(PROJECT_DIR / "input/train.csv.zip")
 X_test = pd.read_csv(PROJECT_DIR / "input/test.csv.zip")
